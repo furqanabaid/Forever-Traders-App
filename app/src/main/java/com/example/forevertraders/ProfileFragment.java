c9893c95cff751c0fe6d206abb24782d61e396e8
dannyz61 <70197304+dannyz61@users.noreply.github.com>
2022-01-20 02:50:06 +0500

    AlertDialog integrated

--- app/src/main/java/com/example/forevertraders/ProfileFragment.java
+++ app/src/main/java/com/example/forevertraders/ProfileFragment.java
@@ -1,9 +1,11 @@
 package com.example.forevertraders;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -58,11 +60,21 @@ public class ProfileFragment extends Fragment {
     logout.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-                fauth.signOut();
-                Intent in = new Intent(new Intent(getContext(),loginActivity.class));
-                in.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                getActivity().finish();
-                startActivity(in);
+            AlertDialog.Builder builder=new AlertDialog.Builder(getContext());
+            builder.setMessage("Do you want to Logout ?");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    fauth.signOut();
+                    Intent in = new Intent(new Intent(getContext(),loginActivity.class));
+                    in.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    getActivity().finish();
+                    startActivity(in);
+                }
+            }).setNegativeButton("CANCEL",null);
+            AlertDialog alert= builder.create();
+            alert.show();
+
         }
     });
 
