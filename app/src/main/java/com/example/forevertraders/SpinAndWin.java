542ed22b9185da3d00a0240ec464c5b3a688683d
furqanabaid <furqanabaid@gmail.com>
2022-01-21 00:14:32 +0500

    Adding Spin and Win Activity

---
+++ app/src/main/java/com/example/forevertraders/SpinAndWin.java
@@ -0,0 +1,14 @@
+package com.example.forevertraders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpinAndWin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spin_and_win);
+    }
+}
--- app/src/main/java/com/example/forevertraders/addToCart.java
+++ app/src/main/java/com/example/forevertraders/addToCart.java
@@ -1,14 +1,19 @@
 package com.example.forevertraders;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,93 +28,110 @@ import com.google.firebase.database.FirebaseDatabase;
 import static com.example.forevertraders.App.CHANNEL_1_ID;
 
 public class addToCart extends AppCompatActivity {
-    TextView title_atc,price_atc,description_atc,catagory_atc,countText;
+    TextView title_atc, price_atc, description_atc, catagory_atc, countText;
     ImageView im;
     private NotificationManagerCompat notificationManager;
     FirebaseAuth fauth;
     String image;
     float id_product;
     DatabaseReference myRef;
-    int count=1;
+    int count = 1;
     Toolbar toolbar;
     private String id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_cart);
         //setTitle("Add to Cart");
 
-        ActionBar actionBar=getSupportActionBar();
+        ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Add to cart");
-actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
-        countText=findViewById(R.id.countText);
-        fauth=FirebaseAuth.getInstance();
-        title_atc=findViewById(R.id.title_atc);
-        price_atc=findViewById(R.id.price_atc);
-        description_atc=findViewById(R.id.description_atc);
-        catagory_atc=findViewById(R.id.catagory_atc);
-        im=findViewById(R.id.image_atc);
+        countText = findViewById(R.id.countText);
+        fauth = FirebaseAuth.getInstance();
+        title_atc = findViewById(R.id.title_atc);
+        price_atc = findViewById(R.id.price_atc);
+        description_atc = findViewById(R.id.description_atc);
+        catagory_atc = findViewById(R.id.catagory_atc);
+        im = findViewById(R.id.image_atc);
         notificationManager = NotificationManagerCompat.from(this);
-        Intent intent=getIntent();
-        id_product=intent.getFloatExtra("id",0);
+        Intent intent = getIntent();
+        id_product = intent.getFloatExtra("id", 0);
         title_atc.setText(intent.getStringExtra("title"));
         price_atc.setText(intent.getStringExtra("price"));
         description_atc.setText(intent.getStringExtra("description"));
         catagory_atc.setText(intent.getStringExtra("catagory"));
-        image=intent.getStringExtra("url");
+        image = intent.getStringExtra("url");
         Glide.with(this).load(image).into(im);
     }
 
     public void btn_minus(View view) {
-        if(count<=1){
-            count=1;
-        }
-        else{
-            count=count-1;
-            countText.setText(""+count);
+        if (count <= 1) {
+            count = 1;
+        } else {
+            count = count - 1;
+            countText.setText("" + count);
         }
     }
 
     public void btn_plus(View view) {
-        count=count+1;
-        countText.setText(""+count);
+        count = count + 1;
+        countText.setText("" + count);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public void addToCart(View view) {
-        String instance="https://forevertraders-f504f-default-rtdb.firebaseio.com/";
+        String instance = "https://forevertraders-f504f-default-rtdb.firebaseio.com/";
         FirebaseDatabase database = FirebaseDatabase.getInstance(instance);
         myRef = database.getReference("Users");
-        myRestApiModel model=new myRestApiModel();
-        String title_fb=title_atc.getText().toString();
+        myRestApiModel model = new myRestApiModel();
+        String title_fb = title_atc.getText().toString();
         model.setTitle(title_fb);
         model.setImage(image);
         model.setCategory(catagory_atc.getText().toString());
         model.setPrice(price_atc.getText().toString());
         model.setDescription(description_atc.getText().toString());
         model.setQuantity(count);
-        id=myRef.push().getKey();
+        id = myRef.push().getKey();
         model.setId(id_product);
         myRef.child(fauth.getCurrentUser().getUid()).child("Carts").child(id).setValue(model);
         //Notification
-        Intent activityIntent = new Intent(this, CartFragment.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this,
-                0, activityIntent, 0);
+//        Intent activityIntent = new Intent(this, CartFragment.class);
+//        PendingIntent contentIntent = PendingIntent.getActivity(this,
+//                0, activityIntent, 0);
         String title = title_fb;
         String message = "New item added to your cart!";
 
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
-                .setSmallIcon(R.drawable.ic_one)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .setColor(Color.BLUE)
-                .setContentIntent(contentIntent)
-                .setAutoCancel(true)
-                .setOnlyAlertOnce(true)
-                .build();
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setAutoCancel(true)
+                        .setSmallIcon(R.drawable.ic_one)
+                        .setContentTitle("Notifications Example")
+                        .setContentText("This is a test notification");
 
-        notificationManager.notify(1, notification);
+        Intent notificationIn = new Intent(this, addToCart.class);
+        PendingIntent contentIn = PendingIntent.getActivity(this, 0, notificationIn,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIn);
+        NotificationChannel channel=new NotificationChannel("id","notification",NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationManager manager1=getSystemService(NotificationManager.class);
+manager1.createNotificationChannel(channel);
+        // Add as notification
+        NotificationManagerCompat manager = NotificationManagerCompat.from(addToCart.this);
+        manager.notify(0, builder.build());
+
+//       ` Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+//                .setSmallIcon(R.drawable.ic_one)
+//                .setContentTitle(title)
+//                .setContentText(message)
+//                .setPriority(NotificationCompat.PRIORITY_HIGH)
+//                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+//                .setColor(Color.BLUE)
+//                .build();
+//
+//        notificationManager.notify(0, notification);
+//    }`
     }
 }
