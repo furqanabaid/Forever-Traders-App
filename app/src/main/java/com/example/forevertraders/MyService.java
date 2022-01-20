b96bdb62e5e2f4002c1d3b08970c7498cd3b2d89
furqanabaid <furqanabaid@gmail.com>
2022-01-21 01:15:23 +0500

    Services added

---
+++ app/src/main/java/com/example/forevertraders/MyService.java
@@ -0,0 +1,38 @@
+package com.example.forevertraders;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
+    private MediaPlayer mediaPlayer;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(getApplicationContext(), "Music is playing!", Toast.LENGTH_SHORT).show();
+        mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.music);
+        mediaPlayer.setLooping(false);
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+    }
+}
--- app/src/main/java/com/example/forevertraders/SpinAndWin.java
+++ app/src/main/java/com/example/forevertraders/SpinAndWin.java
@@ -2,13 +2,30 @@ package com.example.forevertraders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SpinAndWin extends AppCompatActivity {
-
+    LottieAnimationView lottieAnimationView;
+    Intent serviceIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spin_and_win);
+        lottieAnimationView=findViewById(R.id.spinANdWin_animation);
+        serviceIntent=new Intent(getApplicationContext(),MyService.class);
+    }
+
+    public void startService(View view) {
+        lottieAnimationView.playAnimation();
+        startService(serviceIntent);
+    }
+
+    public void stopService(View view) {
+        lottieAnimationView.pauseAnimation();
+        stopService(serviceIntent);
     }
 }
